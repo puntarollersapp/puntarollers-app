147cb9c0fed55db2219b9d64de19c93bef1685ba
puntarollersapp <puntarollers.app@gmail.com>
2026-05-04 03:20:34 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,9 @@ import React from "react"
 
 export default function Home() {
   return (
-    <div className="space-y-8 px-4 pb-10">
+    <div className="px-4 py-6 space-y-6">
 
-      {/* HERO */}
-      <div className="text-center space-y-2">
+      <div className="text-center">
         <h1 className="text-2xl font-bold text-white">
           No es solo patinar, es pertenecer
         </h1>
@@ -14,45 +13,40 @@ export default function Home() {
         </p>
       </div>
 
-      {/* INSCRIPCIONES */}
-      <div className="space-y-4">
-
+      <div>
         <a href="https://form.jotform.com/Claudinio/inscripcioneskids">
-          <img src="/banner-kids.png" className="rounded-xl w-full" />
+          <img src="/banner-kids.png" style={{borderRadius: "12px", width: "100%"}} />
         </a>
+      </div>
 
+      <div>
         <a href="https://form.jotform.com/Claudinio/Inscripciones2026">
-          <img src="/banner-adultos.png" className="rounded-xl w-full" />
+          <img src="/banner-adultos.png" style={{borderRadius: "12px", width: "100%"}} />
         </a>
-
       </div>
 
-      {/* PLATAFORMAS */}
-      <div className="space-y-4">
-
+      <div>
         <a href="https://puntarollerscard.com/">
-          <img src="/banner-prcard.png" className="rounded-xl w-full" />
+          <img src="/banner-prcard.png" style={{borderRadius: "12px", width: "100%"}} />
         </a>
-
-        <a href="https://rollermap.vercel.app/">
-          <img src="/banner-rollermap.png" className="rounded-xl w-full" />
-        </a>
-
       </div>
 
-      {/* ALIANZA */}
-      <div className="space-y-4">
+      <div>
+        <a href="https://rollermap.vercel.app/">
+          <img src="/banner-rollermap.png" style={{borderRadius: "12px", width: "100%"}} />
+        </a>
+      </div>
 
+      <div>
         <a href="https://chat.whatsapp.com/EmQnKWP0T6o62Pln03omq0">
-          <img src="/banner-alianza.png" className="rounded-xl w-full" />
+          <img src="/banner-alianza.png" style={{borderRadius: "12px", width: "100%"}} />
         </a>
+      </div>
 
-        <a href="https://www.instagram.com/alianzaroller">
-          <div className="text-center text-sm text-gray-400 underline">
-            Ver Instagram de Alianza
-          </div>
+      <div style={{textAlign: "center"}}>
+        <a href="https://www.instagram.com/alianzaroller" style={{color: "#aaa", fontSize: "14px"}}>
+          Ver Instagram de Alianza
         </a>
-
       </div>
 
     </div>
