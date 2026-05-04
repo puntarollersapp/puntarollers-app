36bc26808d4ac8d3f9b835fdbdfd1568909455ad
puntarollersapp <puntarollers.app@gmail.com>
2026-05-03 23:31:34 -0300

    Update AppLayout.jsx

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,24 +1,16 @@
 import Header from '../components/Header'
 import BottomNav from '../components/BottomNav'
 import MessagePopup from '../components/MessagePopup'
-import { mensajesGlobales } from '../lib/mockData'
+import { useNavigate } from 'react-router-dom'
 
-export default function AppLayout({ children }) {
-  const mensaje = mensajesGlobales.find(m => m.visible)
-
+export default function AppLayout({ children, title, showBack = false }) {
+  const navigate = useNavigate()
   return (
-    <div className="min-h-screen bg-[#050508] text-white flex flex-col">
-
-      <Header />
-
-      <main className="flex-1 px-4 pt-4 pb-24">
-        {children}
-      </main>
-
+    <div className="min-h-screen bg-pr-black pb-24">
+      <Header title={title} showBack={showBack} onBack={() => navigate(-1)} />
+      <main>{children}</main>
       <BottomNav />
-
-      <MessagePopup mensaje={mensaje} />
-
+      <MessagePopup />
     </div>
   )
 }
