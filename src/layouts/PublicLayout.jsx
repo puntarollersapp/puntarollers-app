ea08ab93199e58f86e5a3c750cf8b36b1058b49d
puntarollersapp <puntarollers.app@gmail.com>
2026-05-04 12:13:36 -0300

    Update PublicLayout.jsx

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,16 +1,7 @@
-import React from "react"
-
 export default function PublicLayout({ children }) {
   return (
-    <div className="min-h-screen bg-[#0a0a0a] text-white">
-
-      {/* CONTENEDOR PREMIUM */}
-      <div className="max-w-md mx-auto px-4 py-6">
-
-        {children}
-
-      </div>
-
+    <div className="min-h-screen bg-pr-black">
+      {children}
     </div>
   )
 }
