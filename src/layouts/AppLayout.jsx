65dca35e120d24f2cb0b276a21da1f422ae4c43f
puntarollersapp <puntarollers.app@gmail.com>
2026-05-03 22:59:11 -0300

    Create AppLayout.jsx

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from '../components/Header'
+import BottomNav from '../components/BottomNav'
+import MessagePopup from '../components/MessagePopup'
+import { mensajesGlobales } from '../lib/mockData'
+
+export default function AppLayout({ children }) {
+  const mensaje = mensajesGlobales.find(m => m.visible)
+
+  return (
+    <div className="min-h-screen bg-[#050508] text-white flex flex-col">
+
+      <Header />
+
+      <main className="flex-1 px-4 pt-4 pb-24">
+        {children}
+      </main>
+
+      <BottomNav />
+
+      <MessagePopup mensaje={mensaje} />
+
+    </div>
+  )
+}
